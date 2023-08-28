a4f37aa0e5a46e406197eb484c57b60fd267d51e
cloudy1gor <savelyev7979@gmail.com>
2023-08-28 21:42:10 +0300

    [fix]:changed rofi, dwm, starship...

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -8,7 +8,6 @@ uptime=$(uptime -p | sed -e 's/up //g')
 shutdown=""
 reboot=""
 lock=""
-suspend=""
 logout="󰍃"
 
 # Variable passed to rofi
@@ -17,49 +16,15 @@ options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
 chosen="$(echo "$options" | $rofi_command -p "UP - $uptime" -dmenu -selected-row 2)"
 case $chosen in
     $shutdown)
-    ans=$($HOME/.config/rofi/confirm.sh)
-    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]] || [[ $ans == "н" ]] || [[ $ans == "Н" ]]; then
         sudo poweroff
-    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]] || [[ $ans == "т" ]] || [[ $ans == "Т" ]]; then
-        exit
-        else
-            rofi -theme ~/.config/rofi/askpass.rasi
-        fi
         ;;
     $reboot)
-    ans=$($HOME/.config/rofi/confirm.sh) 
-    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]] || [[ $ans == "н" ]] || [[ $ans == "Н" ]]; then
-        reboot
-    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]] || [[ $ans == "т" ]] || [[ $ans == "Т" ]]; then    
-        exit
-        else
-            rofi -theme ~/.config/rofi/askpass.rasi
-        fi
+        sudo reboot
         ;;
     $lock)
-        ~/.config/bspwm/scripts/i3lock-fancy/i3lock-fancy.sh
-        ;;
-    $suspend)
-    ans=$($HOME/.config/rofi/confirm.sh)
-    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]] || [[ $ans == "н" ]] || [[ $ans == "Н" ]]; then
-        mpc -q pause
-        amixer set Master mute
-        betterlockscreen --suspend
-        #betterlockscreen --suspend -u /usr/share/backgrounds/"$Background"    
-    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]] || [[ $ans == "т" ]] || [[ $ans == "Т" ]]; then    
-        exit
-        else
-            rofi -theme ~/.config/rofi/askpass.rasi
-        fi
+        ~/.local/bin/i3lock-fancy/i3lock-fancy.sh
         ;;
     $logout)
-    ans=$($HOME/.config/rofi/confirm.sh)
-    if [[ $ans == "yes" ]] || [[ $ans == "YES" ]] || [[ $ans == "y" ]] || [[ $ans == "н" ]] || [[ $ans == "Н" ]]; then
         bspc quit
-    elif [[ $ans == "no" ]] || [[ $ans == "NO" ]] || [[ $ans == "n" ]] || [[ $ans == "т" ]] || [[ $ans == "Т" ]]; then    
-        exit
-        else
-            rofi -theme ~/.config/rofi/askpass.rasi
-        fi
         ;;
 esac
--- Theme&Cursor/Readme.md
+++ Theme&Cursor/Readme.md
@@ -1,6 +1,6 @@
 ## To install:
 
 ```sh
-sudo tar xvf capitaine-cursors-r3.tar.gz -C /usr/share/icons/
 sudo tar xvf WhiteSur-Light-solid.tar.xz -C /usr/share/themes/
+sudo cp -r Future-cursor /usr/share/icons/
 ```
