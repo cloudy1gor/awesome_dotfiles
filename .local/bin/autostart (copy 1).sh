e22cd58b5718c4d71523e724d3f4ccc00ed91e14
cloudy1gor <savelyev7979@gmail.com>
2023-10-05 21:12:37 +0300

    changed screenshot

---
+++ .local/bin/autostart (copy 1).sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+hours=$(date +%H)
+
+# Check if the current hour is greater than or equal to 17 (5 PM) or less than 9 (9 AM)
+if (( hours >= 17 || hours < 9 )); then
+    restart=( "redshift" "-P" "-O" "4200" )
+else 
+    restart=( "redshift" "-P" "-O" "5000" )
+fi
+
+declare -a apps=("/usr/bin/dunst" "copyq" "megasync" "telegram-desktop" "safeeyes" "espanso service start --unmanaged" "glava" )
+
+for app in "${apps[@]}"; do
+    if [[ $(pidof "${app}") ]]; then
+        pkill "${app}"
+    fi
+    sleep 0.1
+    eval "${app}" &
+done
+
+# Check if the "redshift" command should be launched
+if [[ ${#restart[@]} -gt 0 ]]; then
+    pkill "redshift"
+    sleep 0.1
+    eval "${restart[*]}" &
+fi
+
