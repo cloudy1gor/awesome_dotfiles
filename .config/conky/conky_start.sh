a282aaa25cc5fb2b1fcf949744718d7714e595eb
cloudy1gor <savelyev7979@gmail.com>
2024-01-07 19:45:56 +0200

    fix: changed conky

--- .config/conky/conky_start.sh
+++ .config/conky/conky_start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if pgrep -x "conky" >/dev/null; then
+	pkill -x conky
+fi
+
 conky -q -c ~/.config/conky/conky_cal.conf -p ~/.config/conky/conky_cal.pid &
+sleep 0.3
 conky -q -c ~/.config/conky/conky.conf -p ~/.config/conky/conky.pid &
