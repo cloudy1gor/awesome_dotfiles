d2c738313968bdf010e5a01c50bfcca25775fdca
cloudy1gor <savelyev7979@gmail.com>
2024-01-13 20:51:12 +0200

    fix:edit init.lua

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,6 +1,6 @@
 return {
   -- Set colorscheme to use
-  colorscheme = "gruvbox",
+  colorscheme = "catppuccin",
 
   vim.opt.clipboard:append "unnamedplus",
 
@@ -21,17 +21,65 @@ return {
       setup = function() require "custom.configs.visual-multi" end,
     },
 
+    -- colorscheme catppuccin
     {
-      "ellisonleao/gruvbox.nvim",
-      as = "gruvbox",
+      "catppuccin/nvim",
+      priority = 1000,
+      name = "catppuccin",
+      opts = {
+        integrations = {
+          aerial = true,
+          alpha = true,
+          cmp = true,
+          dashboard = true,
+          flash = true,
+          gitsigns = true,
+          headlines = true,
+          illuminate = true,
+          indent_blankline = { enabled = true },
+          leap = true,
+          lsp_trouble = true,
+          mason = true,
+          markdown = true,
+          mini = true,
+          native_lsp = {
+            enabled = true,
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
+          navic = { enabled = true, custom_bg = "lualine" },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          semantic_tokens = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
+        },
+      },
+    },
+
+    -- colorscheme gruvbox
+    {
+      "sainnhe/gruvbox-material",
       priority = 1000,
       config = function()
-        require("gruvbox").setup {
-          contrast = "hard",
-        }
-        vim.cmd [[colorscheme gruvbox]]
+        vim.o.background = "light"
+        vim.g.gruvbox_material_background = "hard"
+        vim.g.gruvbox_material_foreground = "original"
+        vim.g.gruvbox_material_enable_bold = "1"
+        vim.g.gruvbox_material_enable_italic = "0"
+
+        vim.cmd.colorscheme "gruvbox-material"
       end,
     },
+
     {
       "mattn/emmet-vim",
       ft = { "html", "css", "javascript", "typescript", "php", "twig" },
