741c4dfefd0d6b32550a812e37502177f6b90382
cloudy1gor <savelyev7979@gmail.com>
2025-02-15 21:47:33 +0200

    updated the autostart script

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
+# Get the current hour
 hours=$(date +%H)
 
 # Check if the current hour is greater than or equal to 17 (5 PM) or less than 9 (9 AM)
 if ((hours >= 17 || hours < 9)); then
-	restart=("redshift" "-P" "-O" "4000")
+    restart=("redshift" "-P" "-O" "4000")
 else
-	restart=("redshift" "-P" "-O" "4700")
+    restart=("redshift" "-P" "-O" "4700")
 fi
 
-declare -a apps=("bash ~/.config/conky/conky_start.sh" "/usr/bin/dunst" "copyq" "megasync" "telegram-desktop" "safeeyes" "espanso service start --unmanaged" "glava")
+# Start or restart redshift
+if [[ ${#restart[@]} -gt 0 ]]; then
+    pkill "redshift"
+    sleep 0.1
+    "${restart[@]}" &
+fi
 
+# List of apps to start
+declare -a apps=(
+    "bash ~/.config/conky/conky_start.sh"
+    "/usr/bin/dunst"
+    "copyq"
+    "megasync"
+    "telegram-desktop -startintray"
+    "safeeyes --no-start"
+    "espanso service start --unmanaged"
+    "glava"
+)
+
+# Start or restart apps
 for app in "${apps[@]}"; do
-	if [[ $(pidof "${app}") ]]; then
-		pkill "${app}"
-	fi
-	sleep 0.1
-	eval "${app}" &
+    exe_name=$(echo "$app" | awk '{print $1}')
+    if pidof "$exe_name" > /dev/null; then
+        pkill "$exe_name"
+    fi
+
+    sleep 0.1
+    eval "$app" &
 done
 
-# Check if the "redshift" command should be launched
-if [[ ${#restart[@]} -gt 0 ]]; then
-	pkill "redshift"
-	sleep 0.1
-	eval "${restart[*]}" &
-fi
